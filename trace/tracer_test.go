db8347d5115a8251db115db40b5ebe5eb3a0e7d0
joniyjoniy <joniyjoniylo@gmail.com>
2016-03-07 12:27:38 +0000

    [add] logを追加

--- trace/tracer_test.go
+++ trace/tracer_test.go
@@ -1,1 +1,23 @@
-hoge
+package trace
+import (
+	"bytes"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("Newからの戻り値がnilです")
+	} else {
+		tracer.Trace("こんにちは、traceパッケージ")
+		if buf.String() != "こんにちは、traceパッケージ\n" {
+			t.Errorf("'%s'という誤った文字列が出力されました", buf.String())
+		}
+	}
+}
+
+func TestOff(t *testing.T) {
+	var silentTracer Trace = Off()
+	silentTracer.Trace("データ")
+}
